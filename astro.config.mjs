21705e301d47d9b95b2e015eb3459d4a80f501f3
erixnpai <erianhernandez97@gmail.com>
2024-12-05 15:52:44 -0600

    deploy con vercel

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [mdx(), sitemap()],
+	output: 'server',
+  	adapter: vercel(),
+});
